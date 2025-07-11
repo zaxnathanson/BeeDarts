66ee8531d8bf85c06fabb73fc0b47bd8332413c7
brody-brody <brody.silva@mymail.champlain.edu>
2025-07-11 16:29:54 -0400

    rendering a hex grid, scripts folder and prefabs folder

---
+++ Bee Darts FR/Assets/Scripts/Grid/HexRenderer.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public struct Hex
+{
+    public int q;
+    public int r;
+
+    public Hex(int q, int r)
+    {
+        this.q = q;
+        this.r = r;
+    }
+}
+
+public class HexRenderer : MonoBehaviour
+{
+    [SerializeField] private GameObject hexPrefab;
+
+    [Header("Grid Settings")]
+
+    [SerializeField] private int hexGridRadius = 5;
+    [SerializeField] private float hexRadius = 2;
+    [SerializeField] private float random;
+
+
+    private void Awake()
+    {
+        GenerateHexGrid(hexGridRadius);
+    }
+
+    // geenerating a hex grid radially from a center point. radius is how many rings outward
+    private void GenerateHexGrid(float hexGridRadius)
+    {
+        //looping over axial q coordinates from -radius to radius
+        for (int q = -(int)(hexGridRadius); q <= hexGridRadius; q++)
+        {
+            int r1 = Mathf.Max(-(int)(hexGridRadius), -q - (int)(hexGridRadius));
+            int r2 = Mathf.Min((int)(hexGridRadius), -q + (int)(hexGridRadius));
+
+            for (int r = r1; r <= r2; r++)
+            {
+                Vector3 pos = HexToWorld(q, r, hexRadius);
+                Instantiate(hexPrefab, pos, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    private Vector3 HexToWorld(int q, int r, float hexRadius)
+    {
+        float x = hexRadius * 3f / 2f * q;
+        float y = Random.Range(-random, random); //randomness y value here -------------------------------------------
+        float z = hexRadius * Mathf.Sqrt(3f) * (r + q / 2f);
+        return new Vector3(x, y, z);
+    }
+}
